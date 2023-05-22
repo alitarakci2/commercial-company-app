6ed50e6ff8b22bbe01dbbbfcab0664b9f666e2e1
Ali <101870797+alitarakci2@users.noreply.github.com>
2023-05-22 04:06:36 +0300

    commit 3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,52 @@
 import logo from "../images/logo.svg";
 import { pageLinks, socialLinks } from "../data";
+import React, { useState, useEffect, useRef } from 'react';
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const navRef = useRef();
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (navRef.current && !navRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} className="nav-logo" alt="backroads" />
-          <button type="button" className="nav-toggle" id="nav-toggle">
+          <button
+            type="button"
+            className={`nav-toggle ${isOpen ? "nav-open" : ""}`}
+            id="nav-toggle"
+            onClick={() => setIsOpen(!isOpen)}
+          >
             <i className="fas fa-bars"></i>
           </button>
         </div>
-        {/* <!-- left this comment on purpose --> */}
 
-        <ul className="nav-links" id="nav-links">
-          {pageLinks.map((link) => {
-            return (
-              <li key={link.id}>
-                <a href={link.href} className="nav-link">
-                  {link.text}
-                </a>
-              </li>
-            );
-          })}
+        <ul className={`nav-links ${isOpen ? 'show-links' : ''}`} ref={navRef}>
+          {pageLinks.map((link) => (
+            <li key={link.id}>
+              <a href={link.href} className="nav-link" onClick={handleLinkClick}>
+                {link.text}
+              </a>
+            </li>
+          ))}
         </ul>
 
         <ul className="nav-icons">
